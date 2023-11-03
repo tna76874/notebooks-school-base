a3f68948e024f841e0502e7e6405e81fa0995fd2
lmh <dev@hilberg.eu>
2023-11-03 22:33:31 +0100

    adding simple webserver to tunnel

--- modules/tnamodules/tunnel.py
+++ modules/tnamodules/tunnel.py
@@ -8,12 +8,49 @@ import time
 import os
 import qrcode
 from IPython.display import display, Image
+import subprocess
+import signal
 
+class SimpleHTTPServer:
+    def __init__(self, port=8080):
+        self.port = port
+        self.process = None
+        self.home_directory = os.path.join(os.path.expanduser("~"), "http")
+
+
+    def start(self):
+        # Erstelle das Home-Verzeichnis, wenn es nicht existiert
+        if not os.path.exists(self.home_directory):
+            os.makedirs(self.home_directory)
+
+        # Wechsle zum Home-Verzeichnis
+        os.chdir(self.home_directory)
+        
+        # Baue den Befehl zusammen
+        command = f"python -m http.server {self.port}"
+
+        # Starte den Server im Hintergrund und erhalte die PID
+        self.process = subprocess.Popen(command, shell=True)
+
+        # Speichere die PID in einer Datei
+        with open("/tmp/server.pid", "w") as pid_file:
+            pid_file.write(str(self.process.pid))
+
+    def stop(self):
+        if self.process:
+            # Lese die PID aus der Datei
+            with open("/tmp/server.pid", "r") as pid_file:
+                pid = int(pid_file.read().strip())
+
+            # Beende den Prozess
+            os.kill(pid, signal.SIGTERM)
+            self.process = None
 
 class CloudflaredTunnelManager:
     def __init__(self, file_path='/tmp/srv.txt', port=8080):
         self.file_path = file_path
         self.port = port
+        self.http = SimpleHTTPServer(port=self.port)
 
     def check_srv(self):
         if not os.path.exists(self.file_path):
@@ -34,11 +71,14 @@ class CloudflaredTunnelManager:
 
         img = qr.make_image(fill_color="black", back_color="white")
         display(img)
+        
+    def stop_tunnel(self):
+        for i in range(2): os.system('pkill cloudflared')
 
     def start_tunnel(self):
         srv = self.check_srv()
         if not srv:
-            os.system('pkill cloudflared')
+            self.stop_tunnel()
             time.sleep(4)
             os.system(f'tunnel {self.port}')
             time.sleep(4)
